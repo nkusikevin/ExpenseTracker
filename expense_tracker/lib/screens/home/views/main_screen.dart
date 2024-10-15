b19d8cce5be8db8adf5291b597eaa5473d32b2b5
nkusikevin <nkusikvnhart@gmail.com>
2024-10-15 11:55:47 +0200

    added home page and overview card

---
+++ expense_tracker/lib/screens/home/views/main_screen.dart
@@ -0,0 +1,171 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Row(
+                children: [
+                  Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Container(
+                        width: 50,
+                        height: 50,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle, color: Colors.yellow[700]),
+                      ),
+                      Icon(
+                        CupertinoIcons.person_fill,
+                        color: Colors.yellow[900],
+                      ),
+                    ],
+                  ),
+                  const SizedBox(width: 10),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Welcome!',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 12,
+                              color: Theme.of(context).colorScheme.outline)),
+                      Text('John Doe',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                              color: Theme.of(context).colorScheme.onSurface)),
+                    ],
+                  ),
+                ],
+              ),
+              Container(
+                child: Icon(CupertinoIcons.settings,
+                size: 30,
+                    color: Theme.of(context).colorScheme.outline),
+              ),
+            ],
+          ),
+         const SizedBox(height: 20),
+         Container(
+            width: MediaQuery.of(context).size.width ,
+            height: MediaQuery.of(context).size.width/2,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.1),
+                  offset: const Offset(0, 10),
+                  blurRadius: 10,
+                ),
+              ],
+              gradient: LinearGradient(
+                colors: [
+                  Theme.of(context).colorScheme.tertiary,
+                  Theme.of(context).colorScheme.secondary,
+                  Theme.of(context).colorScheme.primary,
+                ],
+                transform: const GradientRotation(pi / 4)
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+              Text('Total Balance',
+                  style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 14,
+                      color: Colors.white)),
+                Text("\$ 10,000.00",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 40,
+                      color: Colors.white)),
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Row(
+                      
+                      children: [
+                        Container(
+                          width: 20,
+                          height: 20,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.white30),
+                          child: const Icon(CupertinoIcons.arrow_up,
+                              size: 12, color: Colors.white),
+                        ),
+                        const SizedBox(width: 10),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('Income',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w400,
+                                      fontSize: 14,
+                                      color: Colors.white)),
+                              Text("\$ 1,000.00",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 14,
+                                      color: Colors.white)),
+                            ],
+                          ),
+                      ],
+                    ),
+                    Row(
+                        children: [
+                          Container(
+                            width: 20,
+                            height: 20,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle, color: Colors.white30),
+                            child: const Icon(CupertinoIcons.arrow_down,
+                                size: 12, color: Colors.white),
+                          ),
+                          const SizedBox(width: 10),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('Expense',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 14,
+                                      color: Colors.white)),
+                              Text("\$ 500.00",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 14,
+                                      color: Colors.white)),
+                            ],
+                          ),
+                        ],
+                      ),
+                  ],
+                )
+                ],
+              ),
+            ),
+         )
+        ],
+      ),
+    );
+  }
+}

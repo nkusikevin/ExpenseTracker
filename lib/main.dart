3e42ec05250185a6242bafe4475db511891af116
nkusikevin <nkusikvnhart@gmail.com>
2024-10-16 15:41:15 +0200

    added firebase options

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,14 @@ import 'package:expense_tracker/app.dart';
 import 'package:expense_tracker/simple_bloc_observer.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+   await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   Bloc.observer = SimpleBlocObserver();
 
   runApp(const MyApp());
